3a04dc0a5ec0ddc9d46d9bd14ad9b1f57102bb63
Ulturaa <138582087+Ulturaa@users.noreply.github.com>
2025-06-25 16:48:50 +0800

    Updated layout for resultspane

--- DLSUFoodMap/src/ResultsPane.java
+++ DLSUFoodMap/src/ResultsPane.java
@@ -4,13 +4,13 @@ import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.ImageIcon;
 import javax.swing.JCheckBox;
+import javax.swing.BoxLayout;
 
 import java.awt.Color;
 import java.awt.Image;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.event.ActionListener;
 
 public class ResultsPane extends JPanel {
@@ -18,14 +18,14 @@ public class ResultsPane extends JPanel {
     private JLabel title, score, path, timeComp, memComp, optimality;
 
     public ResultsPane(String type) {
-        this.setLayout(new FlowLayout());
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
         this.title = new JLabel("Search Alg " + type);
-        this.score = new JLabel("Score: 0");
-        this.path = new JLabel("Path: ");
-        this.timeComp = new JLabel("Time Complexity: ");
-        this.memComp = new JLabel("Memory Complexity: ");
-        this.optimality = new JLabel("Optimal: Yes");
+        this.score = new JLabel("Score:\n   0");
+        this.path = new JLabel("Path:\n   ABCD");
+        this.timeComp = new JLabel("Time Complexity:\n   0s");
+        this.memComp = new JLabel("Memory Complexity:\n   0mp");
+        this.optimality = new JLabel("Optimal:\n   Yes");
         this.setOpaque(false);
 
         this.add(title);
@@ -37,10 +37,10 @@ public class ResultsPane extends JPanel {
     }
 
     public void UpdateTexts(int score, String path, String tc, String mc, String optimality){
-        this.score.setText("Score: " + score);
-        this.path.setText("Path: " + path);
-        this.timeComp.setText("Time Complexity: " + tc);
-        this.memComp.setText("Memory Complexity: " + mc);
-        this.optimality.setText("Optimal: Yes" + optimality);
+        this.score.setText("Score:\n   " + score);
+        this.path.setText("Path:\n   " + path);
+        this.timeComp.setText("Time Complexity:\n   " + tc);
+        this.memComp.setText("Memory Complexity:\n   " + mc);
+        this.optimality.setText("Optimal:\n   " + optimality);
     }
 }
